d9894354dd298262abe718ecf3c460dc9acfc504
jon-erikklint <jonerik.klint@gmail.com>
2017-03-26 20:38:42 +0300

    9

--- Verkkokauppa1/src/main/java/ohtu/verkkokauppa/Kauppa.java
+++ Verkkokauppa1/src/main/java/ohtu/verkkokauppa/Kauppa.java
@@ -7,13 +7,6 @@ public class Kauppa {
     private Ostoskori ostoskori;
     private IViitegeneraattori viitegeneraattori;
     private String kaupanTili;
-
-    public Kauppa() {
-        varasto = Varasto.getInstance();
-        pankki = Pankki.getInstance();
-        viitegeneraattori = Viitegeneraattori.getInstance();
-        kaupanTili = "33333-44455";
-    }
     
     public Kauppa(IVarasto varasto, IPankki pankki, IViitegeneraattori viitegeneraattori){
         this.varasto = varasto;
